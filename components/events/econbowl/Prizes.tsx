7dd1d6fa8ec4912077b00e4ec2d3cbd11740df99
Sepandar <sepandar24@gmail.com>
2025-09-09 18:02:45 -0700

    updated econbowl with prizes

--- components/events/econbowl/Prizes.tsx
+++ components/events/econbowl/Prizes.tsx
@@ -3,61 +3,49 @@ import CountUpOnce from './../../utility/CountUp'
 
 export default function Prizes() {
   return (
-    <div className="bg-yei-primary-main">
-      <div className="overflow-hidden relative max-w-7xl mx-auto py-6 px-4 sm:pb-10 sm:px-6 lg:px-8 lg:pb-12">
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-center z-10">
-          <div className="max-w-4xl mx-auto text-center">
-            <h2 className="text-4xl font-extrabold text-white sm:text-5xl sm:pb-4">
-              <span className="text-green-200">Cash</span> Prizes
-            </h2>
-            {/* <p className="mt-3 text-xl text-green-200 sm:mt-4">
+    <div className="bg-yei-secondary-brighter">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+            Cash Prizes
+          </h2>
+          {/* <p className="mt-3 text-xl text-indigo-200 sm:mt-4">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Repellendus repellat laudantium.
             </p> */}
-          </div>
-          <dl className="mt-8 text-center md:max-w-3xl items-end md:mx-auto grid grid-cols-3 gap-2 sm:gap-4">
-            <div className="flex flex-col order-2 bg-yei-primary-darkest pb-20 pt-4">
-              <dt className="order-2 mt-1 sm:mt-3">
-                <span className="text-sm sm:text-lg font-bold text-green-100">
-                  Champion
-                </span>
-              </dt>
-              <dd className="text-2xl sm:text-2xl md:text-6xl font-black text-white">
-                $<CountUpOnce number={500} />
-              </dd>
-            </div>
-            <div className="flex flex-col mt-10 md:mt-0 order-1 bg-yei-primary-darker pb-14 pt-4">
-              <dt className="order-2 mt-1 sm:mt-3">
-                <span className="text-sm sm:text-lg font-semibold text-green-100">
-                  2nd Place
-                </span>
-              </dt>
-              <dd className="text-xl sm:text-xl md:text-5xl font-black text-green-100">
-                $<CountUpOnce number={400} />
-              </dd>
-            </div>
-            <div className="flex flex-col mt-10 md:mt-0 order-3 bg-yei-primary-darker pb-8 pt-4">
-              <dt className="order-2 mt-1 sm:mt-3">
-                <span className="text-sm sm:text-lg font-semibold text-green-100">
-                  3rd Place
-                </span>
-              </dt>
-              <dd className="text-xl sm:text-xl md:text-5xl font-black text-green-200">
-                $<CountUpOnce number={250} />
-              </dd>
-            </div>
-          </dl>
-          <div className="inline-flex w-auto mx-auto flex-row justify-center items-center mt-10 text-center rounded-full px-6 py-3 border-4 border-yei-primary-darker">
-            <dt className="order-2 ml-2">
-              <span className="text-sm sm:text-lg font-semibold text-green-100">
-                4th Place
+        </div>
+        <dl className="mt-10 text-center sm:max-w-3xl items-center sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
+          <div className="flex flex-col sm:order-2">
+            <dt className="order-2 mt-3">
+              <span className="inline-flex items-center px-5 py-1 rounded-full text-2xl font-medium bg-indigo-100 text-indigo-800">
+                1st
               </span>
             </dt>
-            <dd className="text-xl sm:text-xl md:text-4xl font-black text-green-200">
-              $<CountUpOnce number={100} />
+            <dd className="order-1 text-7xl font-extrabold text-white">
+              $<CountUpOnce number={700} />
             </dd>
           </div>
-        </div>
+          <div className="flex flex-col mt-10 sm:mt-0 sm:order-1">
+            <dt className="order-2 mt-3">
+              <span className="inline-flex items-center px-4 py-0.5 rounded-full text-xl font-medium bg-indigo-200 text-indigo-800">
+                2nd
+              </span>
+            </dt>
+            <dd className="order-1 text-6xl font-extrabold text-gray-200">
+              $<CountUpOnce number={500} />
+            </dd>
+          </div>
+          <div className="flex flex-col mt-10 sm:mt-0 sm:order-3">
+            <dt className="order-2 mt-3">
+              <span className="inline-flex items-center px-4 py-0.5 rounded-full text-md font-semibold bg-indigo-200 text-indigo-800">
+                3rd
+              </span>
+            </dt>
+            <dd className="order-1 text-5xl font-extrabold text-gray-300">
+              $<CountUpOnce number={300} />
+            </dd>
+          </div>
+        </dl>
       </div>
     </div>
   )
