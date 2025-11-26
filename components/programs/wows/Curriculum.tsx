1aa57be422301c7b1b34ec4520da6d7012268487
Sepandar <sepandar24@gmail.com>
2025-11-25 17:13:18 -0800

    Revert "update WOWS curriculum"
    This reverts commit 6732efbc725ad98d285ca56c9b578be42091462c.

--- components/programs/wows/Curriculum.tsx
+++ components/programs/wows/Curriculum.tsx
@@ -25,12 +25,6 @@ export default function Curriculum() {
           content=""
           slideLink="https://docs.google.com/presentation/d/e/2PACX-1vRaHD3SsBESQS6YUSARNbxEG6ScZAtzgJf_3_uCx_OduAPhhlb7ds5uifEEdxDA4SckoQrUOBMc7wfY/embed"
         />
-        <CurriculumSection
-          title="Women in Finance"
-          darkerBg={true}
-          content=""
-          slideLink="https://docs.google.com/presentation/d/1Ekc3NYvH2QCO7kACwjNjWZO3N-rRqOR7fqHdRIKfTOs/embed"
-        />
       </div>
     </div>
   )
