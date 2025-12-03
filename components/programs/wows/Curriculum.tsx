f6c46d05db809907c5507ed5bfc72de2f46ae498
Sepandar <sepandar24@gmail.com>
2025-12-02 17:34:19 -0800

    added credits for the WOWS slides

--- components/programs/wows/Curriculum.tsx
+++ components/programs/wows/Curriculum.tsx
@@ -11,25 +11,25 @@ export default function Curriculum() {
         </div>
         <CurriculumSection
           title="Investment Banking"
-          content=""
+          content="Created by Prasanna Chandankhede and Michael Zhao"
           slideLink="https://docs.google.com/presentation/d/e/2PACX-1vSlS0FOH8UOIKj9p6bUjpG5gbZQYfCcH7scoKAToRi26iZCMB3NtPjS0uEuFk6r1ZZezerQ_5IjFlve/embed"
         />
         <CurriculumSection
           title="Women In Venture Capital"
           darkerBg={true}
           slideLink="https://docs.google.com/presentation/d/e/2PACX-1vR4PqlXbLg3J4pLR_DCTPxE4ZmhF7KsSLZejo-7iAIeKWlfA2Y9ih6rEY08YatmzS45H1LG39Zq-KIC/embed"
-          content=""
+          content="Created by Prasanna Chandankhede and Michael Zhao"
         />
         <CurriculumSection
           title="Financial Literacy for Women"
-          content=""
+          content="Created by Sricharan Bala"
           slideLink="https://docs.google.com/presentation/d/e/2PACX-1vRaHD3SsBESQS6YUSARNbxEG6ScZAtzgJf_3_uCx_OduAPhhlb7ds5uifEEdxDA4SckoQrUOBMc7wfY/embed"
         />
         <CurriculumSection
           title="Women in Finance"
           darkerBg={true}
           slideLink="https://docs.google.com/presentation/d/1Ekc3NYvH2QCO7kACwjNjWZO3N-rRqOR7fqHdRIKfTOs/embed#slide=id.p"
-          content=""
+          content="Created by Manesh Yandra"
         />
       </div>
     </div>
