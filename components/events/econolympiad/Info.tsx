72d31f3b401349841189867a1e54e5f746d0cee8
Sepandar <sepandar24@gmail.com>
2026-02-11 10:07:48 -0800

    updated econolympiad

--- components/events/econolympiad/Info.tsx
+++ components/events/econolympiad/Info.tsx
@@ -147,17 +147,17 @@ export default function Info() {
         <div className="mt-8 prose prose-green prose-lg text-gray-500 mx-auto">
           <h2>Presentation Round</h2>
           <p>
-            Competitors will have up to three week to create their presentation up
-            to 10 minutes in duration. There will be a prompt that focuses on
+            Competitors will have up to three week to create their presentation
+            up to 10 minutes in duration. There will be a prompt that focuses on
             Macroeconomics concepts. Your group will have a chance to research
             the prompts, take a position, provide facts to support your
             argument, and use your reasoning to tie it all together.
           </p>
-          <h2>Debate Round</h2>
+          {/*<h2>Debate Round</h2>
           <p>
             Top teams will participate in a debate with a prompt different from
             previous choices, determining the final EconOlympiad winners.
-          </p>
+          </p>*/}
 
           {/* <h2>Team Matching: A New Way To Find Members</h2>
           <p>
