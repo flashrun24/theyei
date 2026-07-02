31966327bb547e75300ff5958df709ee9b93d6d2
shlokacharya <64059790+shlokacharya@users.noreply.github.com>
2026-07-02 16:15:27 -0700

    Add new curriculum section and update existing ones
    Added a new CurriculumSection for '50 Things You Shouldn't Learn the Hard Way' and updated existing sections. Removed duplicate section for '50 Things You Shouldn't Learn the Hard Way'.

--- components/programs/flip/Curriculum.tsx
+++ components/programs/flip/Curriculum.tsx
@@ -6,51 +6,7 @@ export default function Curriculum() {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:py-20 sm:px-6 lg:px-8">
-        <CurriculumSection
-          title="9-Module Middle School Curriculum"
-          content="Seeing the need for financial literacy exposure at earlier stages in
-        the school system, the FLIP team, led by Saqib Saiyed and with Alex
-        Xie as the curriculum designer, has worked to develop this 9-module
-        Middle School Financial Literacy curriculum for free use around the
-        world. This course has no requirements or prerequisites, so feel free
-        to start your financial literacy journey with these lessons! For more
-        in-depth FinLit learning, schools can use our 16-week High School FLIP
-        curriculum below."
-          slideLink="https://docs.google.com/presentation/d/e/2PACX-1vTGYfIoP4N5xo-WB3wDmGw0ogikHYvEAjJ4GK8096g1XKFJ4ybA6PlA_G1pHTTAn5dx6NFQQkOQxgUs/embed"
-          folderLink="https://drive.google.com/drive/folders/1U2G8m_cr2rwGYTsDKvGPImbmX2jPsUGV"
-        />
-        <CurriculumSection
-          title="16-Week High School Curriculum"
-          darkerBg={true}
-          folderLink="https://drive.google.com/drive/folders/1LkHpqZE3igG7tkEGAM8XoxozgSuO7CB0"
-          slideLink="https://docs.google.com/presentation/d/e/2PACX-1vRtiO_Jb6KgAQwLIeCIQ8l7tSfrYPv_JpEuB-X0F2dRH02QcJv4mfkdD20W-8zYZoKF5zSLUzV_yu4g/embed"
-          content={
-            <p>
-              While EconClubs must apply to be accepted to our FLIP program,
-              every YEI EconClub is provided with the{' '}
-              <b>16-week FLIP curriculum</b>, which can be used to teach club
-              members about financial literacy. The FLIP curriculum has
-              approximately 30 hours worth of content and covers topics such as
-              budgeting, loans, credit and debit cards, 401(k), retirement,
-              interest, savings, investing, and insurance. retirement, interest,
-              savings, investing, and insurance.
-            </p>
-          }
-        />
-        <CurriculumSection
-          title="FLIP Net Price Calculator"
-          slideLink="https://docs.google.com/presentation/d/1Sem27iYDqh21a8BQMtuoyj97uKdsN4YB6U5UyvSGEmc/embed#slide=id.g4dfce81f19_0_45"
-          content="To further support college-bound students, the Youth Economics Initiative FLIP Department has made a Net Price Calculator Guide to help students with their college financing needs. Created by FLIP Partnership Manager Prasanna Chandankhede, this guide is intended to demystify college financing resources options."
-          caption="Preview of Net Price Calculator"
-        />
-        <CurriculumSection
-          title="Financial Literacy in the Age of AI & Digital Money"
-          darkerBg={true}
-          //folderLink=""
-          slideLink="https://docs.google.com/presentation/d/1BraTKVxccaYmnoXqS18Q-oUEO3lz_8jAIuldcCNdQWQ/embed#slide=id.g11a1ccd9bf3_0_102"
-          content="Recognizing the massive shift toward digital money and artificial intelligence, the FLIP team has developed a brand-new 7-week curriculum — Financial Literacy in the Age of AI & Digital Money — to prepare students for the future of finance. This 7-week course, designed by Sricharan Bala (FLIP Curriculum Manager) under Prasanna Chandankhede (VP of FLIP)'s supervision, introduces students to the essentials of digital finance, AI, and fintech, helping them understand how technology is reshaping money and opportunity."
-        />
-        <CurriculumSection
+         <CurriculumSection
          title="50 Things You Shouldn't Learn the Hard Way"
          imageLink="https://static.tildacdn.net/tild3533-6633-4363-b561-623239313561/og-image-50things.jpg"
          caption="A Teen's User Manual for Money"
@@ -70,6 +26,51 @@ export default function Curriculum() {
            </p>
            }
          />
+        <CurriculumSection
+          title="9-Module Middle School Curriculum"
+          darkerBg={true}
+          content="Seeing the need for financial literacy exposure at earlier stages in
+        the school system, the FLIP team, led by Saqib Saiyed and with Alex
+        Xie as the curriculum designer, has worked to develop this 9-module
+        Middle School Financial Literacy curriculum for free use around the
+        world. This course has no requirements or prerequisites, so feel free
+        to start your financial literacy journey with these lessons! For more
+        in-depth FinLit learning, schools can use our 16-week High School FLIP
+        curriculum below."
+          slideLink="https://docs.google.com/presentation/d/e/2PACX-1vTGYfIoP4N5xo-WB3wDmGw0ogikHYvEAjJ4GK8096g1XKFJ4ybA6PlA_G1pHTTAn5dx6NFQQkOQxgUs/embed"
+          folderLink="https://drive.google.com/drive/folders/1U2G8m_cr2rwGYTsDKvGPImbmX2jPsUGV"
+        />
+        <CurriculumSection
+          title="16-Week High School Curriculum"   
+          folderLink="https://drive.google.com/drive/folders/1LkHpqZE3igG7tkEGAM8XoxozgSuO7CB0"
+          slideLink="https://docs.google.com/presentation/d/e/2PACX-1vRtiO_Jb6KgAQwLIeCIQ8l7tSfrYPv_JpEuB-X0F2dRH02QcJv4mfkdD20W-8zYZoKF5zSLUzV_yu4g/embed"
+          content={
+            <p>
+              While EconClubs must apply to be accepted to our FLIP program,
+              every YEI EconClub is provided with the{' '}
+              <b>16-week FLIP curriculum</b>, which can be used to teach club
+              members about financial literacy. The FLIP curriculum has
+              approximately 30 hours worth of content and covers topics such as
+              budgeting, loans, credit and debit cards, 401(k), retirement,
+              interest, savings, investing, and insurance. retirement, interest,
+              savings, investing, and insurance.
+            </p>
+          }
+        />
+        <CurriculumSection
+          title="FLIP Net Price Calculator"
+          darkerBg={true}
+          slideLink="https://docs.google.com/presentation/d/1Sem27iYDqh21a8BQMtuoyj97uKdsN4YB6U5UyvSGEmc/embed#slide=id.g4dfce81f19_0_45"
+          content="To further support college-bound students, the Youth Economics Initiative FLIP Department has made a Net Price Calculator Guide to help students with their college financing needs. Created by FLIP Partnership Manager Prasanna Chandankhede, this guide is intended to demystify college financing resources options."
+          caption="Preview of Net Price Calculator"
+        />
+        <CurriculumSection
+          title="Financial Literacy in the Age of AI & Digital Money"
+          //folderLink=""
+          slideLink="https://docs.google.com/presentation/d/1BraTKVxccaYmnoXqS18Q-oUEO3lz_8jAIuldcCNdQWQ/embed#slide=id.g11a1ccd9bf3_0_102"
+          content="Recognizing the massive shift toward digital money and artificial intelligence, the FLIP team has developed a brand-new 7-week curriculum — Financial Literacy in the Age of AI & Digital Money — to prepare students for the future of finance. This 7-week course, designed by Sricharan Bala (FLIP Curriculum Manager) under Prasanna Chandankhede (VP of FLIP)'s supervision, introduces students to the essentials of digital finance, AI, and fintech, helping them understand how technology is reshaping money and opportunity."
+        />
+       
       </div>
     </div>
   )
