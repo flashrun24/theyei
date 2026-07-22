a37bc81cdaf2bd410591b23ebbb74471c41b2237
shlokacharya <64059790+shlokacharya@users.noreply.github.com>
2026-07-21 23:10:52 -0700

    Replace Yolanda Perez with Ana Alvarenga in wows

--- data/people/wows.ts
+++ data/people/wows.ts
@@ -1,10 +1,10 @@
 const wows = [
   {
-    name: 'Yolanda Perez',
-    date: 'June 19, 2025',
-    occupation:
-      'Senior Vice President, International Wealth Advisor, Truist Wealth',
-    bio: 'Yolanda Perez is a Senior Vice President and International Wealth Advisor at Truist Wealth, where she is responsible for the development of relationships with international individuals, families and companies. Yolanda has over 18 years of experience working with high-net-worth clients in various roles throughout her career in North America and Europe. Prior to joining Truist, she held senior roles in prominent financial institutions in Miami, New York and London in their wealth management and investment banking divisions. She is fluent in Spanish and French and earned her LLM (Master of Laws) in international law, with a specialization in U.S. and transnational laws, from the University of Miami School of Law. She also graduated with a Bachelor of Laws degree from the University of London in England and received her Bachelor of Arts degree from McGill University. She is a native of Montreal, Canada. Yolanda serves as a Trust and Estate Practitioner (TEP) and is a member of numerous local community organizations, including the Finance & Investment Committee of the Broward College Foundation Board and the Planned Giving Advisory Boards for the University of Miami and the NSU Art Museum. Yolanda is passionate about financial literacy education and has done seminars and workshops for professional athletes in various sports and for organizations, such as the Miami Marlins, Minnesota Twins and The Philadelphia Phillies.',
+    name: 'Ana Alvarenga',
+    date: 'July 25, 2026',
+    occupation: 'Vice President, Intrepid Investment Bankers',
+    bio: 'Ana Alvarenga is a Vice President at Intrepid Investment Bankers, where she advises companies, creditors, and investors on liquidity and capital structure challenges. She previously worked at Centre Partners Management and Sun Orchard Juicery, and studied at Williams College, where she played varsity soccer and lacrosse.',
+    hasImage: false,
   },
 ]
 
