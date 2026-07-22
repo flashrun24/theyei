5b97aa33d9a7e56bee26b5ac8e0ac8199d96f042
shlokacharya <64059790+shlokacharya@users.noreply.github.com>
2026-07-21 23:43:32 -0700

    Add Zoom link for Ana Alvarenga

--- data/people/wows.ts
+++ data/people/wows.ts
@@ -5,6 +5,7 @@ const wows = [
     occupation: 'Vice President, Intrepid Investment Bankers',
     bio: 'Ana Alvarenga is a Vice President at Intrepid Investment Bankers, where she advises companies, creditors, and investors on liquidity and capital structure challenges. She previously worked at Centre Partners Management and Sun Orchard Juicery, and studied at Williams College, where she played varsity soccer and lacrosse.',
     hasImage: false,
+    zoomLink: 'https://us06web.zoom.us/j/87569868575',
   },
 ]
 
